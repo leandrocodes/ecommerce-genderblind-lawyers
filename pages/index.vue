5daba9a26a5962b3112df8d412d4f52a4630e676
leandrocodes <leandrovianacodes@gmail.com>
2020-09-06 19:39:11 -0400

    :construction: loading na página index

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="container">
-    <h1 class="title">Gender Blinders</h1>
+    <template v-if="isLoading">
+      <h1>Carregando...</h1>
+    </template>
+    <template v-else>
+      <h1 class="title">Gender Blinders</h1>
 
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link to="/signup"> Sign Up </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/signin"> Sign In </nuxt-link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <nuxt-link to="/signup"> Sign Up </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/signin"> Sign In </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    isLoading: true
+  }),
   beforeCreate() {
     this.$fireAuth.onAuthStateChanged(user => {
       if (user) this.$router.push('/dashboard')
+      else this.isLoading = false
     })
   }
 }
