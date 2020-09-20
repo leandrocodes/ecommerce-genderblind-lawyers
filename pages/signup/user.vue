34b80e836a97c5ecdbc398f20c0a8cec6b11ead7
leandrocodes <leandrovianacodes@gmail.com>
2020-09-19 21:17:44 -0400

    Registro de usuários

--- pages/signup/user.vue
+++ pages/signup/user.vue
@@ -10,8 +10,17 @@
       </ul>
     </nav>
 
-    <section>
-      <form @submit.prevent="signUp">
+    <section class="form__section d-flex jc-center">
+      <form class="d-flex fd-column" @submit.prevent="signUp">
+        <label for="nome" class="mt-4">Nome completo:</label>
+        <input
+          id="nome"
+          v-model="user.name"
+          placeholder="Nome"
+          type="text"
+          name="name"
+        />
+        <label for="email" class="mt-4">Email:</label>
         <input
           id="email"
           v-model="user.email"
@@ -19,6 +28,15 @@
           type="email"
           name="email"
         />
+        <label for="phone" class="mt-4">Telefone:</label>
+        <input
+          id="phone"
+          v-model="user.phone"
+          placeholder="Telefone"
+          type="number"
+          name="phone number"
+        />
+        <label for="password" class="mt-4">Senha:</label>
         <input
           id="password"
           v-model="user.password"
@@ -26,7 +44,7 @@
           type="password"
           name="password"
         />
-        <input type="submit" value="Create User" />
+        <input type="submit" :value="inputValue" class="mt-4" />
       </form>
     </section>
   </div>
@@ -36,20 +54,25 @@
 export default {
   data: () => ({
     user: {
+      name: '',
+      phone: '',
       email: '',
       password: ''
-    }
+    },
+    inputValue: 'Register'
   }),
   methods: {
     async signUp() {
+      this.inputValue = 'Carregando...'
       const { email, password } = this.user
       try {
         await this.$fireAuth.createUserWithEmailAndPassword(email, password)
         await this.$fireStore.collection('users').add({
-          name: 'Asda',
-          surname: 'Fjkl'
+          name: this.user.name,
+          phone: this.user.phone
         })
-        // console.log(id)
+        this.$router.push({ name: 'dashboard' })
+        this.inputValue = 'Register'
       } catch (e) {
         // eslint-disable-next-line no-console
         console.error(e)
@@ -59,4 +82,10 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.form__section {
+  form {
+    width: 30vw;
+  }
+}
+</style>
