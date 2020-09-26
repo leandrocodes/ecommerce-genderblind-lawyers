3479c2e31cabf4a15dad071c8c8a1ad2defa0a1b
leandrocodes <leandrovianacodes@gmail.com>
2020-09-25 20:21:54 -0400

    :art: Nova section hero - mobile first

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,34 @@
       <h1>Carregando...</h1>
     </template>
     <template v-else>
-      <div class="column is-6">
-        <div class="title">
-          <h2 class="fw-normal">Gender Blinders</h2>
-          <h1 class="fw-bold">Lawyers</h1>
-        </div>
-      </div>
-      <!-- <h1 class="title">Gender Blinders <span>Lawyers</span></h1> -->
+      <div class="col-xs-12 col-md-6 p-0">
+        <section class="hero">
+          <div class="hero__title pt-6">
+            <h2 class="fw-normal">Gender Blinders</h2>
+            <h1 class="fw-bold">Lawyers</h1>
+          </div>
 
-      <div class="column is-6">
-        <nav class="txt-upper float-right">
+          <div class="boxes mt-6 pb-2">
+            <div class="row center-xs">
+              <div class="col-xs-8">
+                <div class="black__box pt-6 pb-4 px-4">
+                  <p class="fw-bold">HIGH RATED</p>
+                  <p class="mt-2">Most Recommended</p>
+                </div>
+              </div>
+              <div class="col-xs-8">
+                <div class="black__box pt-6 pb-4 px-4">
+                  <p class="fw-bold">LATEST LAWYERS</p>
+                  <p class="mt-2">Recent Registered</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div class="col-xs-12 col-md-6">
+        <nav class="txt-upper">
           <ul>
             <li>
               <nuxt-link to="/signup"> Sign Up </nuxt-link>
@@ -48,16 +66,32 @@ export default {
   padding-top: 2rem;
   background-color: $primary;
 }
-.title {
-  h1,
-  h2 {
-    text-align: center;
+
+.hero {
+  min-height: 65vh;
+  background-color: rgba($color: $peep, $alpha: 0.15);
+  .hero__title {
+    h1,
+    h2 {
+      text-align: center;
+    }
+    h2 {
+      font-size: 2.25rem;
+    }
+    h1 {
+      font-size: 3rem;
+    }
   }
-  h2 {
-    font-size: 2.25rem;
-  }
-  h1 {
-    font-size: 3rem;
+  .black__box {
+    background-color: black;
+    color: white;
+    text-align: right;
+    text-transform: uppercase;
+    p {
+      &:last-child {
+        font-size: 0.75rem;
+      }
+    }
   }
 }
 </style>
