2094335a1759eb6a870d0b21f6db5b9461bb3f48
leandrocodes <leandrovianacodes@gmail.com>
2020-12-08 20:25:45 -0400

    :tada: lawyer dashboard

---
+++ components/user.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="lawyer__card">
+    <div class="profile__pic">
+      <img
+        :src="require(`~/assets/images/${randomProfile}.png`)"
+        alt=""
+        srcset=""
+      />
+      <div class="status"></div>
+    </div>
+    <main class="profile__main">
+      <div class="profile__name">{{ user.name }}</div>
+      <div class="profile__specification">
+        <i class="mdi mdi-clipboard-outline"></i>
+        {{ randomCause }}
+      </div>
+    </main>
+    <button class="cta__button">
+      <i class="mdi mdi-forum"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+import userImage from '../mixins/userImage'
+export default {
+  mixins: [userImage],
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    causas: ['Causa Trabalhista', 'Causa Cidadã']
+  }),
+  computed: {
+    randomCause() {
+      return this.causas[Math.floor(Math.random() * this.causas.length)]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.lawyer__card {
+  padding: 1rem 1.15rem;
+  width: 85%;
+  background: white;
+  border-left: 10px solid $secondary;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+  box-shadow: 1.5px 1.5px 12px rgba(0, 0, 0, 0.05);
+}
+
+.profile__name {
+  font-weight: bold;
+  font-size: 18px;
+  margin: 0.5rem 0;
+}
+
+.profile__specification {
+  display: flex;
+  align-items: center;
+  color: $dark-text;
+  .mdi {
+    font-size: 26px;
+    margin-right: 2.5px;
+    color: $dark-text;
+  }
+}
+
+.profile__main {
+  .rating {
+    color: #fcc946;
+    font-weight: bolder;
+    display: flex;
+    align-items: center;
+    .mdi-star {
+      font-size: 24px;
+    }
+  }
+}
+
+.cta__button {
+  width: 60px;
+  height: 60px;
+  margin-left: auto;
+  background: $secondary;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 1.5px 1.5px 12px rgba(0, 0, 0, 0.05);
+  .mdi {
+    color: white;
+    font-size: 38px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+.profile__pic {
+  position: relative;
+  margin-right: 10px;
+  img {
+    width: 53px;
+    height: 55px;
+    border-radius: 50%;
+  }
+  .status {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    border: 3px solid white;
+    background-color: #05ff00;
+    position: absolute;
+    bottom: -5px;
+    right: -5px;
+    z-index: 1;
+  }
+
+  .status::before {
+    color: #eee;
+    font-size: 0.25em;
+    padding-left: 4px;
+  }
+}
+</style>
--- pages/lawyer/index.vue
+++ pages/lawyer/index.vue
@@ -1,9 +1,61 @@
 <template>
-  <h1>Lawyer Index</h1>
+  <div class="container">
+    <header>
+      <nav>
+        <nuxt-link to="/"><i class="material-icons">arrow_back</i></nuxt-link>
+        <h1 class="title"><span class="fw-normal">Lawyer</span> Desk</h1>
+      </nav>
+    </header>
+    <div class="lawyer__list">
+      <user v-for="(user, index) in users" :key="index" :user="user" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import user from '../../components/user'
+export default {
+  components: {
+    user
+  },
+  data: () => ({
+    users: []
+  }),
+  created() {
+    this.fetchUsers()
+  },
+  methods: {
+    async fetchUsers() {
+      const users = await this.$fireStore.collection('users').get()
+      if (!users.empty) {
+        users.forEach(user => {
+          for (let i = 0; i < 3; i++) this.users.push(user.data())
+        })
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+header {
+  max-height: 70vh;
+  width: 100%;
+  nav {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-top: 1.25rem;
+    h1 {
+      width: 100%;
+      text-align: center;
+    }
+  }
+}
+.lawyer__list {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem 1rem;
+}
+</style>
